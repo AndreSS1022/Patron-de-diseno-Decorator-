0427eaa6a94ed55662b74a919b0f44e6f784a42f
AndreSS <108243074+AndreSS1022@users.noreply.github.com>
2025-02-24 13:14:32 -0500

    Add files via upload

--- Bebidas.py
+++ Bebidas.py
@@ -1,4 +1,4 @@
-
+# Componente base
 class Bebida:
     def obtener_descripcion(self):
         pass
@@ -6,7 +6,7 @@ class Bebida:
     def calcular_precio(self):
         pass
 
-
+# Componente concreto
 class CafeSimple(Bebida):
     def obtener_descripcion(self):
         return "Café simple"
@@ -14,7 +14,7 @@ class CafeSimple(Bebida):
     def calcular_precio(self):
         return 1000
 
-
+# Decorador base
 class ComplementoBebida(Bebida):
     def __init__(self, bebida):
         self.bebida = bebida
@@ -25,7 +25,7 @@ class ComplementoBebida(Bebida):
     def calcular_precio(self):
         return self.bebida.calcular_precio()
 
-
+# Decoradores concretos
 class Leche(ComplementoBebida):
     def obtener_descripcion(self):
         return f"{self.bebida.obtener_descripcion()}, con leche"
@@ -47,13 +47,15 @@ class Canela(ComplementoBebida):
     def calcular_precio(self):
         return self.bebida.calcular_precio() + 250
 
-
+# Uso
 mi_cafe = CafeSimple()
 print(f"{mi_cafe.obtener_descripcion()} - ${mi_cafe.calcular_precio()}")
+# Café simple - $1.000
 
 mi_cafe_con_leche = Leche(mi_cafe)
 print(f"{mi_cafe_con_leche.obtener_descripcion()} - ${mi_cafe_con_leche.calcular_precio()}")
+# Café simple, con leche - $1.500
 
 mi_cafe_completo = Canela(Azucar(Leche(mi_cafe)))
 print(f"{mi_cafe_completo.obtener_descripcion()} - ${mi_cafe_completo.calcular_precio()}")
-
+# Café simple, con leche, con azúcar, con canela - $2.000
